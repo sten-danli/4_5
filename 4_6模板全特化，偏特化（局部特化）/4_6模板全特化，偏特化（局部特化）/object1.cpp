25a6810bfe0222292cc4f0ba3c6774f83eb6852c
Dan Li <akiraryo@hotmail.com>
2019-09-02 15:50:58 +0200

    //三。函数模板泛化版本：

--- 4_6模板全特化，偏特化（局部特化）/4_6模板全特化，偏特化（局部特化）/object1.cpp
+++ 4_6模板全特化，偏特化（局部特化）/4_6模板全特化，偏特化（局部特化）/object1.cpp
@@ -42,11 +42,67 @@ void TC<double, double>::functest()//单独特化成员函数。
 	cout << "double,double的functest（）特化版本" << endl;
 };
 
+//////////////////////////////////////////////////////////////////////////
+//偏特化版本的泛化版本
+template<typename T, typename U, typename W>
+struct TUW
+{
+	void functest()
+	{
+		cout << "泛化版本" << endl;
+	}
+};
+//偏特化的类模板
+template<typename U>
+struct TUW<int,U,double>//从参数数量上偏特化，我们现在绑定2个类型模板参数，留下一个模板参数。
+{
+	void functest()
+	{
+		cout << "偏特化版本执行" << endl;
+	}
+};
+/////////////////////////////////////////////////////////////////////////////
+
+
+//三。函数模板泛化版本：
+template<typename T, typename U>
+void tfunc(T& tmp1, U& tmp2)
+{
+	cout << "函数泛化版本" << endl;
+	cout << tmp1 << endl;
+	cout << tmp2 << endl;
+
+};
+
+//和篇特化一样，有泛化版本后才可以有全特化版本：
+//函数模板q全特化版本：
+template<>
+void tfunc(int& tmp1, double& tmp2)
+{
+	cout << "________________begin_________________________";
+	cout << "函数全特化版本" << endl;
+	cout << tmp1 << endl;
+	cout << tmp2 << endl;
+	cout << "________________end___________________________";
+}
+
 
 
 
 int main()
 {
+	//调用泛化函数
+
+	int i = 5888;
+	double j = 58.889;
+	tfunc<int, double>(i,j);
+
+	//调用特化函数
+	const char* p = "i am python pro!";
+	string str = "i am c++ pro ";
+	int x = 52888;
+	tfunc(p, x);
+
 	TC<char,int> tcchar;
 	tcchar.functest();//这是一个泛化版本。
 
@@ -56,5 +112,6 @@ int main()
 	TC<double, double> tcdouble;//虽然我们没有给double，double写特化版本类，但是这里调用泛化版本的构造函数。
 	tcdouble.functest();//double,double的functest（）特化版本。因为我们特化了double，double类型的functest类型的functest函数，所以这里调用的是特化的functest（）函数。
 
-
+	TUW<int,char,double> tuw;
+	tuw.functest(); //偏特化版本执行
 }
